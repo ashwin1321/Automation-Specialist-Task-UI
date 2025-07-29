f3355c6491528c71298a4c0de8b9ddee7ca8489b
ashwin1321 <ashwinkhatiwada7@gmail.com>
2025-07-29 07:36:43 +0545

    get item dashboard test

---
+++ tests/crudOperations/06-getItem.test.js
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+import { userLogin } from '../../utils/authenticateHelpers.js';
+import credentials from '../../utils/credentials.js';
+import { filterItem } from '../../utils/allTransactionsHelper.js';
+
+test('Check if the item is present in dashboard or not -Get request validation', async ({ page }) => {
+
+    try {
+        await page.goto('/');
+
+        await userLogin(page, credentials.name, credentials.password);
+
+        await filterItem(page)
+
+        await expect(page.getByText("test data get request")).toBeVisible();
+
+
+    } catch (error) {
+        console.error('something went wroing', error);
+        throw error;
+    }
+});
